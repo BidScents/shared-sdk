9c0f3cae019542e1c38af153dc3a78df7dab5bc2
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-06-16 15:36:55 +0100

    gitignore and main export file setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// uncomment when src/folders have exports
+// // Stores
+// export * from './stores';
+
+// // Hooks
+// export * from './hooks';
+
+// // Utils
+// export * from './utils/currency/format';
+
+// // Types (will be populated as you add them)
+// export type { User, Listing } from './stores/auth';
+// export type { Listing as ListingType } from './stores/listings';
